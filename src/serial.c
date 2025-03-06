5a724c043d719ba334f24d47cadb00a8b6030cf6
Karen Webb <karen.webb@umbralsoftware.co.uk>
2025-03-06 19:30:51 +0000

    Comment serial code

--- src/serial.c
+++ src/serial.c
@@ -15,17 +15,17 @@ static void outb(uint16_t port, uint8_t value) {
 }
 
 void serial_init(void) {
-    outb(COM1 + 1, 0x00);
-    outb(COM1 + 3, 0x80);
-    outb(COM1 + 0, 0x03);
-    outb(COM1 + 1, 0x00);
-    outb(COM1 + 3, 0x03);
-    outb(COM1 + 2, 0xC7);
-    outb(COM1 + 4, 0x0F);
+    outb(COM1 + 1, 0x00); // Disable Interrupts
+    outb(COM1 + 3, 0x80); // Enable DLAB
+    outb(COM1 + 0, 0x01); // Baud Rate Divisor (LSB) 
+    outb(COM1 + 1, 0x00); // Baud Rate Divisor (MSB)
+    outb(COM1 + 3, 0x03); // Disable DLAB, 8 data-bits, 1 stop-bit, 0 parity-bits
+    outb(COM1 + 2, 0xC7); // FIFOs enabled and both cleared, trigger interrupt at 14 bytes
+    outb(COM1 + 4, 0x0B); // Set DTR, RTS, OUT2(connect to IRQ line)
 }
 
 void serial_putc(char c) {
-    while (!(inb(COM1 + 5) & 0x20));
+    while (!(inb(COM1 + 5) & 0x20)); // Wait for transmit buffer to be empty
 
     outb(COM1, c);
 }
